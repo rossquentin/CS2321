cc17e935ddf87809a9ebe6533e63966798c0d609
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 22:41:22 -0400

    Removed unnecessary import

--- Assignment4/Greedy/src/cs2321/FractionalKnapsack.java
+++ Assignment4/Greedy/src/cs2321/FractionalKnapsack.java
@@ -1,30 +1,26 @@
 package cs2321;
 
-import net.datastructures.Entry;
-
 import java.util.Comparator;
 
 /**
  * @author: Quentin Ross
- *
  */
 public class FractionalKnapsack {
-	/**
-	 * Goal: Choose items with maximum total benefit but with weight at most W.
-	 *       You are allowed to take fractional amounts from items.
-	 *       
-	 * @param items items[i][0] is weight for item i
-	 *              items[i][1] is benefit for item i
-	 * @param knapsackWeight    maximum capacity of the knapsack
-	 * @return The maximum total benefit. Please use double type operation. For example 5/2 = 2.5
-	 * 		 
-	 */
+    /**
+     * Goal: Choose items with maximum total benefit but with weight at most W.
+     * You are allowed to take fractional amounts from items.
+     *
+     * @param items          items[i][0] is weight for item i
+     *                       items[i][1] is benefit for item i
+     * @param knapsackWeight maximum capacity of the knapsack
+     * @return The maximum total benefit. Please use double type operation. For example 5/2 = 2.5
+     */
     @TimeComplexity("O(n lg n)")
-	public static double MaximumValue(int[][] items, int knapsackWeight) {
+    public static double MaximumValue(int[][] items, int knapsackWeight) {
         int totalWeight = 0;
         double benefit = 0;
 
-        Comparator<Double> comp = (a, b) -> -1*((Comparable <Double>) a).compareTo(b);
+        Comparator<Double> comp = (a, b) -> -1 * ((Comparable<Double>) a).compareTo(b);
         HeapPQ<Double, Integer> pq = new HeapPQ<>(comp);
 
         // Insert all items into the PQ, with the key being b/w and the value being b.
@@ -38,7 +34,7 @@ public class FractionalKnapsack {
             int currentWeight = pq.min().getValue();
 
             // Add the maximum amount of benefit and weight allowed to the knapsack.
-            benefit += currentValue * Math.min(currentWeight, knapsackWeight-totalWeight);
+            benefit += currentValue * Math.min(currentWeight, knapsackWeight - totalWeight);
             totalWeight += currentWeight;
 
             // Finished with all calculations. Remove maximum element.
@@ -46,6 +42,6 @@ public class FractionalKnapsack {
         }
 
         // Return the total value of the knapsack.
-		return benefit;
-	}
+        return benefit;
+    }
 }
