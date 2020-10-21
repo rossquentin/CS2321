8d87f2a462f642805c0e2f2dbb0e483ce83f0878
BuildTools <unconfigured@null.spigotmc.org>
2020-10-21 15:46:48 -0400

    \bin updated

--- Assignment5/Sorts_Spring20/src/cs2321/QuickSort.java
+++ Assignment5/Sorts_Spring20/src/cs2321/QuickSort.java
@@ -7,25 +7,33 @@ public class QuickSort<E extends Comparable<E>> implements Sorter<E> {
         quickSort(array, 0, array.length - 1);
 	}
 
-	private void quickSort(E[] arr, int low, int high) {
-	    if (low < high) {
-	        int part = partition(arr, low, high);
-	        quickSort(arr, low, part-1);
-	        quickSort(arr, part+1, high);
+	private void quickSort(E[] array, int start, int end) {
+	    if (start < end) {
+	        int part = partition(array, start, end);
+	        quickSort(array, start, part-1);
+	        quickSort(array, part+1, end);
         }
     }
 
-    private int partition(E[] arr, int low, int high) {
-	    E key = arr[high];
-	    int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j].compareTo(key)<= 0) {
+    private int partition(E[] array, int start, int end) {
+	    E pivot = array[end];
+        int i = start;
+        int j = end - 1;
+        while (i <= j) {
+            while (i <= j && array[i].compareTo(pivot) < 0) {
                 i++;
-                swap(arr,i, j);
+            }
+            while (i <= j && array[j].compareTo(pivot) >= 0 ) {
+                j--;
+            }
+            if (i < j) {
+                swap(array, i, j);
+                i++;
+                j--;
             }
         }
-        swap(arr, high, i+1);
-	    return i+1;
+        swap(array, i, end);
+        return i;
     }
 
     private void swap(E[] arr, int i, int j) {
