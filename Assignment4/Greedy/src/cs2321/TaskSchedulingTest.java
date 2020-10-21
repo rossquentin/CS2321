1b86ed92a6bc039b8254650f7d947c6614f649ae
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 22:48:18 -0400

    Added additional tests for other task schedule, as well as an empty task schedule.

--- Assignment4/Greedy/src/cs2321/TaskSchedulingTest.java
+++ Assignment4/Greedy/src/cs2321/TaskSchedulingTest.java
@@ -25,4 +25,32 @@ public class TaskSchedulingTest {
         assertEquals(expected, TaskScheduling.NumOfMachines(arr));
     }
 
+    @Test
+    public void testNumOfMachines3() {
+        int[][] arr = {{1,4}, {6,8}, {10,15}, {15,20}};
+        int expected = 1;
+        assertEquals(expected, TaskScheduling.NumOfMachines(arr));
+    }
+
+    @Test
+    public void testNumOfMachines4() {
+        int[][] arr = {{3,7},{4,7},{6,9},{1,4},{1,3},{2,5},{7,8}};
+        int expected = 3;
+        assertEquals(expected, TaskScheduling.NumOfMachines(arr));
+    }
+
+    @Test
+    public void testNumOfMachines5() {
+        int[][] arr = {{1,4},{1,4},{2,5}, {2,5}};
+        int expected = 4;
+        assertEquals(expected, TaskScheduling.NumOfMachines(arr));
+    }
+
+    @Test
+    public void testNumOfMachinesEmpty() {
+	    int[][] arr = {};
+	    int exmpected = 0;
+	    assertEquals(exmpected, TaskScheduling.NumOfMachines(arr));
+    }
+
 }
