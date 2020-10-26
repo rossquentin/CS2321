1d1d80c7118bb946b21764d830ae206ab41b17a5
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 12:30:45 -0400

    Documented

--- Assignment5/Sorts_Spring20/src/cs2321/MergeSort.java
+++ Assignment5/Sorts_Spring20/src/cs2321/MergeSort.java
@@ -5,44 +5,62 @@ import static java.util.Arrays.copyOfRange;
 public class MergeSort<E extends Comparable<E>> implements Sorter<E> {
 
 	public void sort(E[] array) {
-		// TODO Auto-generated method stub
-
-        if (array.length <= 1) {
-            return;
-        }
-
-        int mid = array.length/2;
-        E[] left = copyOfRange(array, 0, mid);
-        E[] right = copyOfRange(array, mid, array.length);
-
-        sort(left);
-        sort(right);
-
-        merge(left, right, array);
+        mergeSort(array, array.length);
 	}
 
-	private void merge(E[] left, E[] right, E[] arr) {
+    /**
+     * Sorts an array of size n via merge sort
+     * @param array the array to sort
+     * @param n     the size of the array
+     */
+	private void mergeSort(E[] array, int n) {
+	    if (array.length <= 1) {
+	        return;
+        }
+
+	    // Creates two sub arrays of with sizes (mid) and (n - mid)
+        int mid = n/2;
+        E[] left = copyOfRange(array, 0, mid);
+        E[] right = copyOfRange(array, mid, n);
+
+        // Sorts the two sub arrays.
+        mergeSort(left, left.length);
+        mergeSort(right, right.length);
+
+        // Then merges the two arrays together.
+        merge(left, right, array);
+    }
+
+	private void merge(E[] left, E[] right, E[] array) {
+	    int l = left.length;
+	    int r = right.length;
 	    int i = 0;
 	    int j = 0;
 	    int k = 0;
 
-	    while (i < left.length && j < right.length) {
+	    // Loops through elements in the left and right sub arrays until either
+        // sub array's elements are looped through
+	    while (i < l && j < r) {
+
+	        // Inserts the smaller of the two elements into the array.
 	        if (left[i].compareTo(right[j]) < 0) {
-	            arr[k] = left[i++];
+	            array[k] = left[i++];
             }
 	        else {
-	            arr[k] = right[j++];
+	            array[k] = right[j++];
             }
 	        k++;
         }
 
-	    while (i < left.length) {
-	        arr[k] = left[i++];
+	    // Insert all remaining elements in the left sub array into the array.
+	    while (i < l) {
+	        array[k] = left[i++];
 	        k++;
         }
 
-	    while(j < right.length) {
-	        arr[k] = right[j++];
+	    // Insert all remaining elements in the right sub array into the array.
+	    while(j < r) {
+	        array[k] = right[j++];
 	        k++;
         }
     }
--- Assignment5/Sorts_Spring20/src/cs2321/QuickSort.java
+++ Assignment5/Sorts_Spring20/src/cs2321/QuickSort.java
@@ -3,42 +3,81 @@ package cs2321;
 public class QuickSort<E extends Comparable<E>> implements Sorter<E> {
 
 	public void sort(E[] array) {
-		// TODO Auto-generated method stub
         quickSort(array, 0, array.length - 1);
 	}
 
+    /**
+     * Sorts a subarray of size (end - start).
+     * @param array the array to sort
+     * @param start the starting index, inclusive, of the sub array
+     * @param end   the ending index, inclusive, of the sub array
+     */
 	private void quickSort(E[] array, int start, int end) {
+
+	    // While we still have a sub array to parse
 	    if (start < end) {
+	        // Get the pivot index post partitioning.
 	        int part = partition(array, start, end);
+
+	        // Sort both sub array less than or greater than te partition's pivot.
 	        quickSort(array, start, part-1);
 	        quickSort(array, part+1, end);
         }
     }
 
+    /**
+     * Partitions a subarray of size (end - start) where items less than a pivot are left of the pivot
+     * and items greater than the pivot are right of the pivot.
+     * @param array the array to partition
+     * @param start the starting index, inclusive, of the sub array
+     * @param end   the ending index, inclusive, of the sub array
+     * @return      a partition of the sub array
+     */
     private int partition(E[] array, int start, int end) {
+        // Sets the pivot to the last element
 	    E pivot = array[end];
+
+	    // Sets the indices of the range
         int i = start;
         int j = end - 1;
+
+        // While the range >= 0
         while (i <= j) {
+
+            // Check if range condition is violated and if A[i] < A[pivot].
+            // If true, increment i.
             while (i <= j && array[i].compareTo(pivot) < 0) {
                 i++;
             }
+
+            // Check if range condition is violated and if A[j] >= A[pivot].
+            // If true, decrement j.
             while (i <= j && array[j].compareTo(pivot) >= 0 ) {
                 j--;
             }
+
+            // If range >= 0, swap i and j, then increment and decrement respectively.
             if (i < j) {
                 swap(array, i, j);
                 i++;
                 j--;
             }
         }
+
+        // Swap i with the pivot to complete the partition, then return the index of the pivot.
         swap(array, i, end);
         return i;
     }
 
-    private void swap(E[] arr, int i, int j) {
-	    E temp = arr[i];
-	    arr[i] = arr[j];
-	    arr[j] = temp;
+    /**
+     * Swaps two indices in an array.
+     * @param array the array to swap with
+     * @param i     the first index to swap
+     * @param j     the second index to swap
+     */
+    private void swap(E[] array, int i, int j) {
+	    E temp = array[i];
+	    array[i] = array[j];
+	    array[j] = temp;
     }
 }
