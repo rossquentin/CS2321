75a468f120de3f7c93ab25d7f9cce7dafef73492
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 12:30:20 -0400

    Documented

--- Assignment5/Sorts_Spring20/src/cs2321/InPlaceInsertionSort.java
+++ Assignment5/Sorts_Spring20/src/cs2321/InPlaceInsertionSort.java
@@ -6,24 +6,34 @@ public class InPlaceInsertionSort<K extends Comparable<K>> implements Sorter<K> 
 	 * sort - Perform an in-place insertion sort
 	 * @param array - Array to sort
 	 */
-
 	public void sort(K[] array) {
-		// TODO Auto-generated method stub
+        insertionSort(array , array.length);
+	}
 
-        int n = array.length;
-
-        if (n <= 1) {
-            return;
-        }
-
+    /**
+     * Sorts, in place, an array of size n via insertion sort.
+     * @param array the array to sort
+     * @param n     the size of the array
+     */
+	private void insertionSort(K[] array, int n) {
+        // Loop through all elements in the interval [1,n)
         for (int i = 1; i < n; i++) {
+
+            // Loop through all elements in reverse order from [i,0)
             for (int j = i; j > 0 ; j--) {
+
+                // If A[j] < A[j-1], swap A[j] and A[j-1]
                 if (array[j].compareTo(array[j-1]) < 0) {
                     K temp = array[j];
                     array[j] = array[j-1];
                     array[j-1] = temp;
                 }
+
+                // Otherwise we know that the element is already in place.
+                else {
+                    break;
+                }
             }
         }
-	}
+    }
 }
